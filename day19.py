b50fd18015747b288ace0dd75c9a27dd0ca9c023
Reinout Versteeg <rcw.versteeg@avans.nl>
2020-12-18 20:41:56 +0100

    dunder

---
+++ day19.py
@@ -0,0 +1,12 @@
+
+def main():
+    with open('day19.txt', 'r') as f:
+        data = f.readlines()
+
+
+    # print("P1: {}".format(solve(data, r1)))
+    # print("P2: {}".format(solve(data, r2)))
+
+
+if __name__ == "__main__":
+    main()
